152cc3aff3bd2fd6d0c31cdad1aaacd2adf7a478
Alejandro Dighiero <adighiero1@gmail.com>
2023-11-02 18:34:54 -0400

    sign-up page. now I will be able to test user creation

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import LandingPage from "./pages/landingPage";
 import SingleVideoPage from "./pages/singleVideoPage";
 import SignInPage from "./pages/signInPage";
+import SignUpPage from "./pages/signupPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/:id" element={<SingleVideoPage />} />
         <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/store/searchSlice.js
+++ client/src/store/searchSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  query: "",
+  //initial state
+  query: "", //two properties we are working with
   results: [],
 };
 
@@ -10,6 +11,7 @@ const searchSlice = createSlice({
   initialState,
   reducers: {
     updateQuery: (state, action) => {
+      //
       state.query = action.payload;
     },
     updateResults: (state, action) => {
@@ -21,3 +23,31 @@ const searchSlice = createSlice({
 export const selectSearch = (state) => state.search;
 
 export default searchSlice.reducer;
+
+/*
+
+State Initialization: The searchSlice begins by initializing its state using the initialState object. In this case, the initial state contains two properties: query and results.
+
+Reducers: The slice defines two reducers that specify how the state should change in response to different actions.
+
+updateQuery: This reducer updates the query property in the state with the payload provided in the action. When this action is dispatched, it takes the new query value and assigns it to state.query. This allows you to update the search query in your Redux store.
+
+updateResults: This reducer updates the results property in the state with the payload provided in the action. It replaces the previous results with the new results data when this action is dispatched.
+
+Action Creators: By using createSlice, action creators are generated automatically. For the updateQuery and updateResults actions, you don't need to write action creator functions explicitly. They are generated as searchSlice.actions.updateQuery and searchSlice.actions.updateResults. These action creators can be dispatched in your components to update the state.
+
+For example, to update the query property, you can dispatch the updateQuery action as follows:
+
+
+dispatch(searchSlice.actions.updateQuery("New Search Query"));
+Similarly, for updating the results property:
+
+
+dispatch(searchSlice.actions.updateResults(newResultsData));
+Selectors: The slice also provides a selector function, selectSearch, which allows you to access the entire search slice of the Redux store. This selector can be used in your React components to retrieve the search-related data from the store.
+
+For example, you can use selectSearch in a component to access the entire search slice:
+
+
+const searchState = useSelector(selectSearch);
+Then, you can access specific properties like searchState.query and searchState.results.*/
