16a270cdb67c061e5ae873f91220a6f5608e74e6
Alejandro Dighiero <adighiero1@gmail.com>
2023-11-05 12:21:40 -0500

    fixed error with routes. postman

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,51 @@
+// import bcrypt from "bcrypt";
+// import { db } from "../connect.js"; // Import the database connection object
+
+// export const register = (req, res) => {
+//   // Check if the user already exists
+//   const checkUserQuery = "SELECT * FROM users WHERE username = ?";
+//   const username = req.body.username;
+
+//   db.query(checkUserQuery, [username], (err, data) => {
+//     if (err) {
+//       return res.status(500).json(err);
+//     }
+
+//     if (data.length) {
+//       return res.status(409).json("User already exists!");
+//     }
+
+//     // Create a new user
+//     const saltRounds = 10;
+//     bcrypt.genSalt(saltRounds, (saltErr, salt) => {
+//       if (saltErr) {
+//         return res.status(500).json(saltErr);
+//       }
+
+//       bcrypt.hash(req.body.password, salt, (hashErr, hashedPassword) => {
+//         if (hashErr) {
+//           return res.status(500).json(hashErr);
+//         }
+
+//         const insertUserQuery =
+//           "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)";
+
+//         const userValues = [req.body.username, req.body.email, hashedPassword];
+
+//         db.query(insertUserQuery, userValues, (insertErr, userData) => {
+//           if (insertErr) {
+//             return res.status(500).json(insertErr);
+//           }
+
+//           return res.status(200).json("User has been created.");
+//         });
+//       });
+//     });
+//   });
+// };
+
 import bcrypt from "bcrypt";
+import { db } from "../connect.js"; // Import the database connection object
 
 export const register = (req, res) => {
   // Check if the user already exists
@@ -27,13 +74,13 @@ export const register = (req, res) => {
         }
 
         const insertUserQuery =
-          "INSERT INTO users (username, email, password, name) VALUES (?, ?, ?, ?)";
+          "INSERT INTO users (username, email, salt, password_hash) VALUES (?, ?, ?, ?)";
 
         const userValues = [
           req.body.username,
           req.body.email,
+          salt,
           hashedPassword,
-          req.body.name,
         ];
 
         db.query(insertUserQuery, userValues, (insertErr, userData) => {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
-import Express from "express";
+import express from "express"; //express sever
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
-const app = Express();
+import { db } from "./connect.js";
+import authRoutes from "./routes/auth.js";
+const app = express();
+//middlewares
+app.use((req, res, next) => {
+  //whenever we make a request we are going to give an access
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
+});
+app.use(express.json()); // using express that json to be able to send data as an object in all our controllers
+app.use(
+  //to only allow localhost
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: " + err.message);
+  } else {
+    console.log("Database connected successfully");
+  }
+});
+
+app.use(cookieParser());
+app.use("/server/auth", authRoutes);
 app.listen(8800, () => {
   console.log("Server API working!");
 });
